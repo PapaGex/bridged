a952184a1eaa8e80e5d31203dfbfd90bf851977e
Kawekaweau <geeksgeckosandgoodvibes@gmail.com>
2022-08-03 16:26:45 -0400

    rearranged the folders and added more to the header and navbar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+
+
+function Header () {
+    return (
+        <div className="Rectangle-9">
+        <header className="App-header">
+            <span class="Projects">
+            Projects
+            </span>
+            <span class="Faucet">
+            Faucet
+            </span>
+            <span class="Orders">
+            Orders
+            </span>
+        </header>
+        </div>
+    );  
+}
+
+export default Header;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,50 @@
+function Navbar() {
+    return (
+        <div className="Rectangle-8">
+        <div class="Bridged-3"></div>
+        <div class="Frame-2-Background">
+            <div class="Shape"></div>
+            <span class="Carbon-Faucet">
+    Carbon Faucet
+</span>
+        </div>
+        <div class="Shape"><div class="Path"></div></div>
+        <span class="Real-time-Dapp-Emiss">
+    Real-time Dapp Emissions
+</span>
+<div class="Shape"></div>
+        <span class="SDG-Bridge">
+    SDG Bridge
+</span>
+<div class="Shape"></div>
+        <span class="My-Contracts">
+    My Contracts
+</span>
+<div class="Shape"></div>
+        <span class="RetireUnwrap">
+    Retire/Unwrap
+</span>
+<div class="Shape"></div>
+        <span class="ESG-Dashboard">
+    ESG Dashboard
+</span>
+<div class="Shape"></div>
+        <span class="Settings">
+    Settings
+</span>
+
+<span class="Algorand-Mainnet">
+    Algorand Mainnet
+</span>
+<div class="Rectangle">
+    <div class="Ellipse-2"></div>
+    <span class="ET7CW5BYAA">
+    ET7CW5BYAA…
+</span>
+<div class="Vector"></div>
+</div>
+        </div>
+    );
+} 
+
+export default Navbar;
